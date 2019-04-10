60e5e81a627a3cd2bec1af5ebe8a5948bff9cf4a
Florian Stanek <flstanek@googlemail.com>
2019-04-10 14:49:30 +0200

    Fix capture container script

--- capture_container.sh
+++ capture_container.sh
@@ -7,30 +7,28 @@ COMMAND=$1
 CAPTURE_FILENAME=${CAPTURE_TARGET}_${START_TIMESTAMP}.tcpdump
 
 start() {
-    docker build -t tcpdump ./tcpdump > /dev/null
+    docker build -t tcpdump ./tcpdump
     docker run -d \
         --network container:$CAPTURE_TARGET \
         --name $CAPTURE_CONTAINER \
         -v $(pwd)/capture:/capture \
         tcpdump -i eth0 -w /capture/$CAPTURE_FILENAME -U
-    wireshark $(pwd)/capture/$CAPTURE_FILENAME & > /dev/null
+    wireshark $(pwd)/capture/$CAPTURE_FILENAME & 
 }
 
 stop() {
-    docker kill $CAPTURE_CONTAINER > /dev/null
-    docker rm $CAPTURE_CONTAINER > /dev/null
+    docker kill $CAPTURE_CONTAINER 1>/dev/null 2>&1
+    docker rm $CAPTURE_CONTAINER 1>/dev/null 2>&1
 }
 
 stop_all() {
-    docker kill $(docker ps -q -f name=.capture)
-    docker rm $(docker ps -aq -f name=.capture)
+    docker rm --force $(docker ps -aq -f name=.capture) 1>/dev/null 2>&1
 }
 
 case "$COMMAND" in
 "start")
     stop
     start
-    stop
     ;;
 "stop")
     stop
