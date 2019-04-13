5d9938c1368c42d9ecbe382ebf8dd01d1a4b593a
Florian Stanek <flstanek@googlemail.com>
2019-04-13 17:53:30 +0200

    Adding scripts to get and parse EC2 instances (for Ansible)

--- nodes/broker/run.sh
+++ nodes/broker/run.sh
@@ -3,9 +3,9 @@
 docker build -t moquette_build -f Dockerfile.build . && \
 docker run -v moquette-dist:/moquette -v gradle:/root/.gradle moquette_build && \
 docker build -t moquette -f Dockerfile . && \
-docker run -d --rm --name moquette \
+docker run -it --rm --name moquette \
     -v moquette-dist:/moquette \
     -v $(pwd)/config:/moquette/config \
-    -p 1884:1883 \
-    moquette && \
-docker logs -f moquette
+    -p 2884:1884 \
+    -p 2883:1883 \
+    moquette 
