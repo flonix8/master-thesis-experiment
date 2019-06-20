77b7430228eada9fb89bffb2798617e98b170fbe
Florian Stanek <flstanek@googlemail.com>
2019-06-20 18:33:20 +0200

    Add log collection (brokers and clients); Finally implement ref topo

--- nodes/broker/build_image.sh
+++ nodes/broker/build_image.sh
@@ -4,3 +4,4 @@ docker build -t moquette-build_base -f Dockerfile.build . && \
 docker run -v gradle-cache:/root/.gradle --name moquette-build moquette-build_base && \
 docker commit moquette-build moquette-build && \
 docker build -t moquette -t flonix8/moquette -f Dockerfile .
+docker push flonix8/moquette
--- topology/testbed_files/python/topologies.py
+++ topology/testbed_files/python/topologies.py
@@ -10,75 +10,103 @@ def reference_topology(g: Graph):
                                               client_config=[
                                                   pub_config(
                                                       connect_to='cloud1_broker1',
-                                                      topic='cloud-to-edge/control-commands',
-                                                      frequency=50,
-                                                  ),
-                                                  pub_config(
-                                                      connect_to='cloud1_broker1',
-                                                      topic='cloud-to-edge/config-updates',
-                                                      frequency=10,
+                                                      topic='/push-infos/road-conditions',
+                                                      frequency=5,
                                                       payload_size=10000,
                                                   ),
                                                   sub_config(
                                                       connect_to='cloud1_broker1',
-                                                      topic='edge-to-cloud/#',
+                                                      topic='/car-telemetry/aggregated/#',
                                                   ),
                                                   sub_config(
                                                       connect_to='cloud1_broker1',
-                                                      topic='edge-to-edge/data/#',
+                                                      topic='/traffic-control/camera-feed/#',
                                                   ),
                                               ]))
     g.add_node('cloud1_broker1', **node_attrs(role='broker'))
     # Edge nodes
+    g.add_node('edge1', **node_attrs(type='zone'))
     g.add_node('edge1_client1', **node_attrs(role='client',
                                              client_config=[
                                                  pub_config(
                                                      connect_to='edge1_broker1',
-                                                     topic='edge-to-cloud/aggregated-data',
+                                                     topic='/traffic-control/camera-feed/1',
                                                      frequency=20,
-                                                     start_offset=20,
-                                                     runtime=40,
                                                      payload_size=500000,
-                                                 ),
-                                                 sub_config(
-                                                     connect_to='edge1_broker1',
-                                                     topic='cloud-to-edge/#',
-                                                 ),
-                                                 sub_config(
-                                                     connect_to='edge1_broker1',
-                                                     topic='edge-to-edge/data/#',
                                                  ),
                                              ]))
     g.add_node('edge1_client2', **node_attrs(role='client',
                                              client_config=[
                                                  sub_config(
                                                      connect_to='edge1_broker2',
-                                                     topic='cloud-to-edge/#',
+                                                     topic='/car-telemetry/realtime/3',
                                                  ),
                                                  sub_config(
                                                      connect_to='edge1_broker2',
-                                                     topic='edge-to-edge/data/client3'
+                                                     topic='/car-telemetry/realtime/4',
                                                  ),
                                                  pub_config(
                                                      connect_to='edge1_broker2',
-                                                     topic='edge-to-edge/control-commands/client3',
-                                                     frequency=20,
+                                                     topic='/car-telemetry/aggregated/2',
+                                                     frequency=5,
+                                                     payload_size=4000,
+                                                 ),
+                                                 pub_config(
+                                                     connect_to='edge1_broker2',
+                                                     topic='/car-telemetry/realtime/2',
+                                                     frequency=100,
+                                                     payload_size=200,
                                                  ),
                                              ]))
     g.add_node('edge1_client3', **node_attrs(role='client',
                                              client_config=[
                                                  sub_config(
-                                                     connect_to='edge1_broker2',
-                                                     topic='edge-to-edge/control-commands/client3',
+                                                     connect_to='edge1_broker3',
+                                                     topic='/car-telemetry/realtime/2',
+                                                 ),
+                                                 sub_config(
+                                                     connect_to='edge1_broker3',
+                                                     topic='/car-telemetry/realtime/4',
                                                  ),
                                                  pub_config(
-                                                     connect_to='edge1_broker2',
-                                                     topic='edge-to-edge/data/client3',
-                                                     frequency=50,
+                                                     connect_to='edge1_broker3',
+                                                     topic='/car-telemetry/aggregated/3',
+                                                     frequency=5,
+                                                     payload_size=4000,
+                                                 ),
+                                                 pub_config(
+                                                     connect_to='edge1_broker3',
+                                                     topic='/car-telemetry/realtime/3',
+                                                     frequency=100,
+                                                     payload_size=200,
+                                                 ),
+                                             ]))
+    g.add_node('edge1_client4', **node_attrs(role='client',
+                                             client_config=[
+                                                 sub_config(
+                                                     connect_to='edge1_broker3',
+                                                     topic='/car-telemetry/realtime/2',
+                                                 ),
+                                                 sub_config(
+                                                     connect_to='edge1_broker3',
+                                                     topic='/car-telemetry/realtime/3',
+                                                 ),
+                                                 pub_config(
+                                                     connect_to='edge1_broker3',
+                                                     topic='/car-telemetry/aggregated/4',
+                                                     frequency=5,
+                                                     payload_size=4000,
+                                                 ),
+                                                 pub_config(
+                                                     connect_to='edge1_broker3',
+                                                     topic='/car-telemetry/realtime/4',
+                                                     frequency=100,
+                                                     payload_size=200,
                                                  ),
                                              ]))
     g.add_node('edge1_broker1', **node_attrs(role='broker'))
     g.add_node('edge1_broker2', **node_attrs(role='broker'))
+    g.add_node('edge1_broker3', **node_attrs(role='broker'))
 
     # Connect cloud
     g.add_edge('cloud1_broker1', 'cloud1', **edge_attrs(delay=2))
@@ -86,11 +114,14 @@ def reference_topology(g: Graph):
     # Connect edge
     g.add_edge('edge1_client1', 'edge1_broker1', **edge_attrs(delay=2))
     g.add_edge('edge1_client2', 'edge1_broker2', **edge_attrs(delay=2))
-    g.add_edge('edge1_client3', 'edge1_broker2', **edge_attrs(delay=2))
-    g.add_edge('edge1_broker2', 'edge1_broker1', **edge_attrs(delay=2))
+    g.add_edge('edge1_client3', 'edge1_broker3', **edge_attrs(delay=2))
+    g.add_edge('edge1_client4', 'edge1_broker3', **edge_attrs(delay=2))
+    g.add_edge('edge1_broker1', 'edge1_broker2', **edge_attrs(delay=2))
+    g.add_edge('edge1_broker2', 'edge1_broker3', **edge_attrs(delay=2))
+    g.add_edge('edge1', 'edge1_broker1', **edge_attrs())
 
     # Connect to cloud
-    g.add_edge('edge1_broker1', 'cloud1', **edge_attrs(delay=20))
+    g.add_edge('edge1', 'cloud1', **edge_attrs(delay=20))
 
 
 def debug_topology(g: Graph):
